7d941de0f78bc7c09ca995a88a1a2cad900b1e5d
Fermín Olaiz <ferminolaiz@gmail.com>
2014-11-01 16:20:29 -0300

    Módulo wiki

---
+++ class/APIs/Wikipedia.php
@@ -0,0 +1,26 @@
+<?php
+	class Wikipedia
+	{
+		private $Endpoint = 'http://es.wikipedia.org/w/api.php'; // In json?
+
+		public function Search($Title)
+		{
+			$URL = "{$this->Endpoint}?action=query&list=search&srwhat=text&format=json&srsearch={$Title}&continue=";
+
+			$Data = file_get_contents($URL);
+			$Data = json_decode($Data, true);
+
+			$Data = $Data['query']['search'];
+
+			if($Data === array())
+				return false;
+
+			$Data = $Data[0];
+
+			return array
+			(
+				'title' => $Data['title'],
+				'content' => $Data['snippet']
+			);
+		}
+	}
