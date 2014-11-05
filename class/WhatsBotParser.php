bfbc1c5ae21ebdf2bac4c0cf4100792f43a7c659
Fermín Olaiz <ferminolaiz@gmail.com>
2014-11-04 22:03:09 -0300

    Cambiamos los datos de origen
    * Agregamos algunos To Do...

--- class/WhatsBotParser.php
+++ class/WhatsBotParser.php
@@ -12,9 +12,23 @@
 
 		public function ParseTextMessage($Me, $FromG, $FromU, $ID, $Type, $Time, $Name, $Text)
 		{
+			if($FromG != null)
+				$From = array
+				(
+					'from' => 'group',
+					'g' => $FromG,
+					'u' => $FromU
+				);
+			else
+				$From = array
+				(
+					'from' => 'privmsg',
+					'u' => $FromU
+				);
+
 			if($Text[0] == '!')
 			{
-				$Parsed = trim($Text, '!');
+				$Parsed = trim($Text, '!'); // Use substr to only delete first char (!)
 				$Parsed = explode(' ', $Parsed);
 
 				$R = $this->ModuleManager->CallModule
@@ -25,8 +39,7 @@
 					array
 					(
 						'me' => $Me,
-						'fromg' => $FromG,
-						'from' => $FromU,
+						'from' => $From
 						'id' => $ID,
 						'type' => $Type,
 						'time' => $Time,
@@ -34,7 +47,7 @@
 					)
 				);
 
-				if($R == false)
+				if($R == false) // moduleExists() - False es error del eval, en todo caso tambie del isset(Module), pero podemo evitar esto testeando adicionalmente...
 					$this->Whatsapp->sendMessage(($FromG != null) ? $FromG : $FromU, 'Ese módulo no existe...');
 			}
 			else
@@ -51,17 +64,28 @@
 
 						if(in_array($Extension, array('jpg', 'png', 'gif'))) // Others
 						{
-							$Filename = tempnam('.', 'tmp');
-							$Filename2 = $Filename . '.' . $Extension;
+							//if(filesize($URL) < 2097152)
+							//{
+							/*function remote_file_size($url){
+	# Get all header information
+	$data = get_headers($url, true);
+	# Look up validity
+	if (isset($data['Content-Length']))
+		# Return file size
+		return (int) $data['Content-Length'];*/
+								$Filename = tempnam('.', 'tmp');
+								$Filename2 = $Filename . '.' . $Extension;
 
-							file_put_contents($Filename2, file_get_contents($URL));
+								file_put_contents($Filename2, file_get_contents($URL));
 
-							$this->Whatsapp->sendMessageImage(($FromG != null) ? $FromG : $FromU, $Filename2);
+								$this->Whatsapp->sendMessageImage(($FromG != null) ? $FromG : $FromU, $Filename2);
 
-							if(is_file($Filename))
-								unlink($Filename);
-							if(is_file($Filename2))
-								unlink($Filename2);
+								if(is_file($Filename))
+									unlink($Filename);
+								if(is_file($Filename2))
+									unlink($Filename2);
+							//}
+
 						}
 						// OTHER MEDIA TYPES
 					}
