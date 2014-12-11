64c36da277260aa7fd7365ea929563563af447f9
Fermín Olaiz <fermino.github@gmail.com>
2014-12-11 15:38:13 -0300

    Readded StdInputParser ('send' command)

--- class/threads/StdInputParser.php
+++ class/threads/StdInputParser.php
@@ -1,25 +1,39 @@
 <?php
-	class Thread_StdInputParser extends WhatsBotThread
+	class Thread_StdInputParser extends Thread
 	{
+		use WhatsBotThread;
+
 		public function run()
 		{
 			require_once 'class/Utils.php';
 
 			while(true)
 			{
+				Utils::Write('>> ', false);
+
 				$Line = Utils::ReadLine();
 				$Splitted = explode(' ', $Line);
 
+				if(empty($Line))
+					continue;
+
 				switch(strtolower($Splitted[0]))
 				{
-					case 'clean':
-						break;
 					case 'send':
-						if(!empty($Splitted[1]) && !empty($Splitted[2]))
-							$this->SendMessage($Splitted[1], $Splitted[2]);
+						if(!empty($Splitted[1]))
+						{
+							$Text = substr($Line, 5 + strlen($Splitted[1]));
+
+							if($Text !== false)
+								$this->SendMessage($Splitted[1], $Text);
+							else
+								Utils::Write('> You must enter some text...');
+						}
+						else
+							Utils::Write('> You must enter some text...');
 						break;
 					default:
-						Utils::Write('Unrecognized command...');
+						Utils::Write('> Unrecognized command...');
 				}
 			}
 		}
