b4f55750808aef7d55b27db50fbd3d19d9fe7397
Fermín Olaiz <fermino.github@gmail.com>
2014-12-14 14:18:41 -0300

    Some functions added to WhatsBotThread (trait)

--- class/ThreadModel.php
+++ class/ThreadModel.php
@@ -28,8 +28,34 @@
 			self::$Tasks[] = array($Type, $Name, $Params);
 		}
 
+		final protected function SendAudio($To, $Filepath, $StoreURLMedia = false, $Filesize = 0, $Filehash = '')
+		{
+			$this->AddTask(WHATSBOT_WHATSAPP_TASK, 'SendAudioMessage', $To, $Filepath, $StoreURLMedia = false, $Filesize = 0, $Filehash = '');
+		}
+
+		final protected function SendImage($To, $Filepath, $Caption = '', $StoreURLMedia = false, $Filesize = 0, $Filehash = '')
+		{
+			$this->AddTask(WHATSBOT_WHATSAPP_TASK, 'SendImageMessage', $To, $Filepath, $Caption = '', $StoreURLMedia = false, $Filesize = 0, $Filehash = '');
+		}
+
 		final protected function SendMessage($To, $Message, $ID = null)
 		{
 			$this->AddTask(WHATSBOT_WHATSAPP_TASK, 'SendMessage', $To, $Message, $ID);
+		}
+
+		final protected function SendVCard($To, $Name, $VCard)
+		{
+			$this->AddTask(WHATSBOT_WHATSAPP_TASK, 'SendVCard', $To, $Name, $VCard);
+		}
+
+		final protected function SendVideo($To, $Filepath, $Caption = '', $StoreURLMedia = false, $Filesize = 0, $Filehash = '')
+		{
+			$this->AddTask(WHATSBOT_WHATSAPP_TASK, 'SendVideoMessage', $To, $Filepath, $Caption = '', $StoreURLMedia = false, $Filesize = 0, $Filehash = '');
+		}
+
+
+		final protected function SetStatus($Status)
+		{
+			$this->AddTask(WHATSBOT_WHATSAPP_TASK, 'SetStatus', $Status);
 		}
 	}
