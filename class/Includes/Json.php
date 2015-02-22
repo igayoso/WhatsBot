50f3a7ed22b69bf4db45ac8ec1273845694f6cf3
Fermín Olaiz <fermino.github@gmail.com>
2015-02-22 00:36:05 -0300

    Json (class) added

---
+++ class/Includes/Json.php
@@ -0,0 +1,32 @@
+<?php
+	class Json
+	{
+		public static function Read($Filename)
+		{
+			if(is_readable($Filename))
+			{
+				$Data = file_get_contents($Filename);
+
+				if($Data !== false)
+				{
+					$Data = json_decode($Data, true);
+
+					if($Data !== null)
+						return $Data;
+				}
+			}
+
+			return false;
+		}
+
+		public static function Write($Filename, $Data)
+		{
+			$Data = json_encode($Data);
+
+			if($Data !== false)
+				if(file_put_contents($Filename, $Data) === strlen($Data))
+					return true;
+
+			return false;
+		}
+	}
