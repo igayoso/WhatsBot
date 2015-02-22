a8d16e1f4a4f26ca3321f62ff6918f99302c6aac
Fermín Olaiz <fermino.github@gmail.com>
2015-02-22 14:33:05 -0300

    ModuleManagerCaller::CallModule added

--- class/ModuleManagerCaller.php
+++ class/ModuleManagerCaller.php
@@ -1,5 +1,28 @@
 <?php
 	trait ModuleManagerCaller
 	{
-		
+		private function CallModule($Key, $Name, Array $Params)
+		{
+			if($this->ModuleExists($Key, $Name))
+			{
+				$WhatsApp = $this->WhatsApp;
+				$ModuleManager = $this;
+
+				// Lang
+
+				$ModuleData = $this->GetModuleData($Key, $Name);
+
+				if($ModuleData !== false)
+				{
+					if(is_readable($ModuleData['file']))
+					{
+						extract($Params);
+
+						return include $ModuleData['file'];
+					}
+				}
+			}
+
+			return false;
+		}
 	}
