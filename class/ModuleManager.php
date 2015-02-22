21255f2d6d57c6a9052f3339ba7f2528a08cddd2
Fermín Olaiz <fermino.github@gmail.com>
2015-02-22 14:30:25 -0300

    Adding WhatsApp to ModuleManager

--- class/ModuleManager.php
+++ class/ModuleManager.php
@@ -1,9 +1,17 @@
 <?php
 	require_once 'ModuleManagerCore.php';
-
 	require_once 'ModuleManagerCaller.php';
+
+	require_once 'WhatsApp.php';
 
 	class ModuleManager extends ModuleManagerCore
 	{
 		use ModuleManagerCaller;
+
+		private $WhatsApp = null;
+
+		public function __construct(WhatsApp $WhatsApp)
+		{
+			$this->WhatsApp = $WhatsApp;
+		}
 	}
