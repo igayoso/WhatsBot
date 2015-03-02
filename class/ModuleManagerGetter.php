4c8d0b2fdee20e6fc4ec4bcf8d11f2ee67ec70a8
Fermín Olaiz <fermino.github@gmail.com>
2015-03-02 01:27:26 -0300

    ModuleManagerGetter ready :D

--- class/ModuleManagerGetter.php
+++ class/ModuleManagerGetter.php
@@ -1,8 +1,42 @@
 <?php
 	trait ModuleManagerGetter
 	{
-		/*
-		 * GetModules
-		 * GetModuleData
-		 */	
+		public function GetModules($Key)
+		{
+			if($this->KeyExists($Key))
+				return array_keys($this->Modules[$Key]);
+
+			return false;
+		}
+
+		protected function GetModule($Key, $Name)
+		{
+			$Name = strtolower($Name);
+
+			if($this->ModuleExists($Key, $Name))
+				return $this->Modules[$Key][$Name];
+
+			return false;
+		}
+
+
+		public function GetCommandModule($Name)
+		{
+			return $this->GetModule('command', $Name);
+		}
+
+		public function GetDomainModule($Name)
+		{
+			return $this->GetModule('domain', $Name);
+		}
+
+		public function GetExtensionModule($Name)
+		{
+			return $this->GetModule('extension', $Name);
+		}
+
+		public function GetMultiMediaModule($Name)
+		{
+			return $this->GetModule('multimedia', $Name);
+		}
 	}
