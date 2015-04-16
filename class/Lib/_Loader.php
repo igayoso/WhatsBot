ff33b679edf528ca463de5984f48d3eae07e49d1
Fermín Olaiz <fermino.github@gmail.com>
2015-04-16 10:40:57 -0300

    IncludeManager => Lib/_Loader.LoadLibs()

--- class/Lib/_Loader.php
+++ class/Lib/_Loader.php
@@ -1,13 +1,56 @@
 <?php
-	$Libs = array
+	$CoreLibs = array
 	(
 		'Std',
 		'Json',
 		'Config',
 		'Lang',
 
-		'.Admin'
+		'Regex'
 	);
 
-	foreach($Libs as $Lib)
-		require_once "{$Lib}.php";
+	LoadCoreLibs($CoreLibs);
+	
+	#############
+	# Functions #
+	#############
+
+	function LoadCoreLibs($Libs)
+	{
+		foreach($Libs as $Lib)
+			require_once "{$Lib}.php";
+	}
+
+	function LoadLibs()
+	{
+		Std::Out();
+		Std::Out('[INFO] [LIBS] Loading');
+
+		$Libs = Config::Get('Libs');
+
+		if(is_array($Libs))
+		{
+			foreach($Libs as $Lib)
+			{
+				$Path = "class/Lib/.{$Lib}.php";
+
+				if(basename(dirname(realpath($Path))) === 'Lib')
+				{
+					if(is_readable($Path))
+					{
+						// Lint
+
+						require_once $Path;
+					}
+					else
+						Std::Out("[WARNING] [LIBS] Can't load {$Lib}. It is not readable");
+				}
+				else
+					Std::Out("[WARNING] [LIBS] Can't load {$Lib}. It is not in Lib/ folder");
+			}
+
+			Std::Out('[INFO] [LIBS] Ready!');
+		}
+		else
+			Std::Out('[WARNING] [LIBS] Config file is not an array');
+	}
