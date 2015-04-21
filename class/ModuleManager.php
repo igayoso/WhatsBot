cf14121be24f5aeff62aaf56a9b1bb23e6778db1
Fermín Olaiz <fermino.github@gmail.com>
2015-04-20 21:31:58 -0300

    ModuleManager (main class) ready! :D

--- class/ModuleManager.php
+++ class/ModuleManager.php
@@ -1,9 +1,10 @@
 <?php
-	require_once 'WhatsBot.php';
+	require_once 'Lib/_Loader.php';
 
 	require_once 'ModuleManagerCore.php';
 	require_once 'ModuleManagerCaller.php';
 
+	require_once 'WhatsBot.php';
 	require_once 'WhatsApp.php';
 
 	class ModuleManager extends ModuleManagerCore
