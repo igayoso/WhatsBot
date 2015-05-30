2e4e4e5e91324fe6d9e6a44e438307ca334a4232
Fermín Olaiz <fermino.github@gmail.com>
2015-05-29 22:07:01 -0300

    WhatsApp/Message's vars are now defined one p/ line

--- class/WhatsApp/Message.php
+++ class/WhatsApp/Message.php
@@ -1,7 +1,15 @@
 <?php
 	class Message
 	{
-		public $Me, $From, $User, $ID, $Type, $Time, $Name = null;
+		public $Me = null;
+
+		public $From = null;
+		public $User = null;
+
+		public $ID = null;
+		public $Type = null;
+		public $Time = null;
+		public $Name = null;
 
 		public function __construct($Me, $From, $User, $ID, $Type, $Time, $Name)
 		{
