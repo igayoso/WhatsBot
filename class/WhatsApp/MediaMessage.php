09361343a7ccdee57e5e26250eb9c9ca33d8b5f0
Fermín Olaiz <fermino.github@gmail.com>
2015-05-29 22:41:40 -0300

    WhatsApp/MediaMessage added

---
+++ class/WhatsApp/MediaMessage.php
@@ -0,0 +1,26 @@
+<?php
+	require_once 'Message.php';
+
+	class MediaMessage extends Message
+	{
+		public $SubType = null;
+
+		public $URL = null;
+		public $File = null;
+		public $Size = null;
+
+		public $MIME = null;
+		public $Hash = null;
+
+		public function __construct($Me, $From, $User, $ID, $Type, $Time, $Name, $SubType, $URL, $File, $Size, $MIME, $Hash)
+		{
+			parent::__construct($Me, $From, $User, $ID, $Type, $Time, $Name);
+
+			$this->SubType = $SubType;
+			$this->URL = $URL;
+			$this->File = $File;
+			$this->Size = $Size;
+			$this->MIME = $MIME;
+			$this->Hash = $Hash;
+		}
+	}
