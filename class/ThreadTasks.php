5fdd11d714c6e8a78c3a0212fb7bf090ed9bfc5d
Fermín Olaiz <fermino.github@gmail.com>
2015-06-04 10:29:07 -0300

    Threads can WaitForReturn(Var) to get task's return

--- class/ThreadTasks.php
+++ class/ThreadTasks.php
@@ -2,6 +2,7 @@
 	trait ThreadTasks
 	{
 		private $Tasks = 'a:0:{}'; // serialized empty array, beacuse pthreads nullifies non-simple data types
+		private $Return = 'a:0:{}';
 
 		public function GetTasks()
 		{
@@ -24,5 +25,41 @@
 			$this->Tasks = serialize($Tasks);
 
 			$this->Unlock();
+		}
+
+		public function SetReturn($Name, $Value)
+		{
+			$this->Lock();
+
+			$Return = unserialize($this->Return);
+			$Return[$Name] = $Value;
+			$this->Return = serialize($Return);
+
+			$this->Unlock();
+		}
+
+		private function WaitForReturn($Name)
+		{
+			while(true)
+			{
+				$this->Lock();
+
+				$Return = unserialize($this->Return);
+
+				if(isset($Return[$Name]))
+				{
+					$Value = $Return[$Name];
+					unset($Return[$Name]);
+					$this->Return = serialize($Return);
+
+					$this->Unlock();
+
+					return $Value;
+				}
+
+				$this->Unlock();
+
+				sleep(1);
+			}
 		}
 	}
