7e20c875c717739783d6fe76dc5340d39c171cfc
Fermín Olaiz <fermino.github@gmail.com>
2015-06-29 23:18:16 -0300

    LoadLib(Lib) added

--- class/Lib/_Loader.php
+++ class/Lib/_Loader.php
@@ -31,26 +31,33 @@
 		if(is_array($Libs))
 		{
 			foreach($Libs as $Lib)
-			{
-				$Path = "class/Lib/.{$Lib}.php";
-
-				if(basename(dirname(realpath($Path))) === 'Lib')
-				{
-					if(is_readable($Path))
-					{
-						// Lint
-
-						require_once $Path;
-					}
-					else
-						Std::Out("[Warning] [Libs] Can't load {$Lib}. It is not readable");
-				}
-				else
-					Std::Out("[Warning] [Libs] Can't load {$Lib}. It is not in Lib/ folder");
-			}
+				LoadLib($Lib);
 
 			Std::Out('[Info] [Libs] Ready!');
 		}
 		else
 			Std::Out('[Warning] [Libs] Config file is not an array');
+	}
+
+	function LoadLib($Lib)
+	{
+		$Path = "class/Lib/.{$Lib}.php";
+
+		if(basename(dirname(realpath($Path))) === 'Lib')
+		{
+			if(is_readable($Path))
+			{
+				// Lint
+
+				require_once $Path;
+
+				return true;
+			}
+			else
+				Std::Out("[Warning] [Libs] Can't load {$Lib}. It is not readable");
+		}
+		else
+			Std::Out("[Warning] [Libs] Can't load {$Lib}. It is not in Lib/ folder");
+
+		return false;
 	}
