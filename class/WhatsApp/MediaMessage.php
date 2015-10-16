cf75910fda39fda345d04dceadf3638d45d76402
fermino <fermino.github@gmail.com>
2015-10-16 00:32:46 -0300

    MediaMessage->LoadData() added (storage funcs)

--- class/WhatsApp/MediaMessage.php
+++ class/WhatsApp/MediaMessage.php
@@ -12,6 +12,10 @@
 		public $MIME = null;
 		public $Hash = null;
 
+		protected $Data = null;
+
+		private $MediaDirectory = 'media';
+
 		public function __construct($Me, $From, $User, $ID, $Type, $Time, $Name, $SubType, $URL, $File, $Size, $MIME, $Hash)
 		{
 			$this->SubType = $SubType;
@@ -22,7 +26,41 @@
 			$this->Hash = $Hash;
 
 			parent::__construct($Me, $From, $User, $ID, $Type, $Time, $Name);
+
+			$this->LoadData();
 		}
+
+		private function LoadData()
+		{
+			Data::CreateDirectory($this->MediaDirectory);
+
+			$Path = $this->MediaDirectory . DIRECTORY_SEPARATOR . $this->File;
+
+			$this->Data = Data::Get($Path, false, false);
+
+			if(empty($this->Data))
+			{
+				$this->Data = file_get_contents($this->URL);
+
+				if($this->Data !== false)
+				{
+					Data::Set($Path, $this->Data);
+
+					$this->Data = base64_encode($this->Data);
+
+					return true;
+				}
+
+				return false;
+			}
+			else
+				$this->Data = base64_decode($this->Data);
+
+			return true;
+		}
+
+		public function GetData()
+		{ return base64_decode($this->Data); }
 
 		public function GetType()
 		{ return $this->SubType; }
