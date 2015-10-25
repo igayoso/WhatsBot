14e08c4c0f79daf122d488bc7debd51abc32104c
fermino <fermino.github@gmail.com>
2015-10-24 21:21:25 -0300

    Lib/Std will use previous handle (Std::Err() added)

--- class/Lib/Std.php
+++ class/Lib/Std.php
@@ -1,24 +1,49 @@
 <?php
 	class Std
 	{
+		private static $Input = null;
+		private static $Output = null;
+		private static $Error = null;
+
+		public static function Init()
+		{
+			if(!is_resource(self::$Input))
+				if(!is_resource(self::$Input = fopen('php://stdin', 'r')))
+					throw new Exception("Can't open php://stdin in read mode");
+
+			if(!is_resource(self::$Output))
+				if(!is_resource(self::$Output = fopen('php://stdout', 'w')))
+					throw new Exception("Can't open php://stdout in write mode");
+
+			if(!is_resource(self::$Error))
+				if(!is_resource(self::$Error = fopen('php://stderr', 'w')))
+					throw new Exception("Can't open php://stderr in write mode");
+		}
+
 		public static function In()
 		{
-			$In = fopen('php://stdin', 'r');
+			self::Init();
 
-			if($In !== false)
-				return trim(fgets($In));
-
-			return false;
+			return trim(fgets(self::$Input));
 		}
 
 		public static function Out($String = null, $NewLines = 1)
-		{ // printf like, with array as params ?
+		{
+			self::Init();
+
 			for($i = 0; $i < $NewLines; $i++)
 				$String .= PHP_EOL;
 
-			if(file_put_contents('php://stdout', $String) === strlen($String))
-				return true;
+			return fwrite(self::$Output, $String) === strlen($String);
+		}
 
-			return false;
+		public static function Err($String = null, $NewLines = 1)
+		{
+			self::Init();
+
+			for($i = 0; $i < $NewLines; $i++)
+				$String .= PHP_EOL;
+
+			return fwrite(self::$Error, $String) === strlen($String);
 		}
 	}
